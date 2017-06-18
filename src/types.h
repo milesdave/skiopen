c95ebd06f65ebac874db5c72ed29c087e3689bb8
Miles Davis <davis.g.miles@gmail.com>
2017-06-18 16:16:43 +0930

    Added types.h.

---
+++ src/types.h
@@ -0,0 +1,34 @@
+#ifndef TYPES_H_
+#define TYPES_H_
+
+#include <SDL_rect.h>
+typedef SDL_Rect Rect, Hitbox;
+
+struct Vector2
+{
+	int x;
+	int y;
+};
+
+struct Vector2F
+{
+	void operator+=(const Vector2F &v)
+	{
+		x += v.x; y += v.y;
+	}
+
+	void operator-=(const Vector2F &v)
+	{
+		x -= v.x; y -= v.y;
+	}
+
+	void operator*=(float a)
+	{
+		x *= a; y *= a;
+	}
+
+	float x;
+	float y;
+};
+
+#endif
