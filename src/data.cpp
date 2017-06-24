854fa424090222e24c4f6ddd60d04d0fcdb856d4
Miles Davis <davis.g.miles@gmail.com>
2017-06-24 18:10:35 +0930

    Added all sprites to Data class.

--- src/data.cpp
+++ src/data.cpp
@@ -13,14 +13,125 @@ void Data::load()
 	/* Sheets */
 
 	_sheets[SH_PLAYER].load(SH_PLAYER_FILE);
+	_sheets[SH_GOAT].load(SH_GOAT_FILE);
+	_sheets[SH_OBSTACLES].load(SH_OBSTACLES_FILE);
+	_sheets[SH_SIGNS].load(SH_SIGNS_FILE);
+	_sheets[SH_ANIM_SIGN].load(SH_ANIM_SIGN_FILE);
+	_sheets[SH_FONT].load(SH_FONT_FILE);
 
-	/* Sprites */
+	/* Player Sprites */
 
-	_sprites[SP_PLAYER_DOWN].frames = SP_PLAYER_DOWN_FRAMES;
-	_sprites[SP_PLAYER_DOWN].duration = SP_PLAYER_DOWN_DURATION;
-	_sprites[SP_PLAYER_DOWN].loop = SP_PLAYER_DOWN_LOOP;
+	_sprites[SP_PLAYER_DOWN].frames = PLAYER_FRAMES;
+	_sprites[SP_PLAYER_DOWN].duration = PLAYER_DURATION;
+	_sprites[SP_PLAYER_DOWN].loop = PLAYER_LOOP;
 	_sprites[SP_PLAYER_DOWN].sheet = &_sheets[SH_PLAYER];
-	_sprites[SP_PLAYER_DOWN].generateFrames(SPRITE_SIZE, SPRITE_SIZE, SP_PLAYER_DOWN_ROW);
+	_sprites[SP_PLAYER_DOWN].generateFrames(SPRITE_SIZE, SPRITE_SIZE,
+		SP_PLAYER_DOWN);
+
+	_sprites[SP_PLAYER_LEFT1].frames = PLAYER_FRAMES;
+	_sprites[SP_PLAYER_LEFT1].duration = PLAYER_DURATION;
+	_sprites[SP_PLAYER_LEFT1].loop = PLAYER_LOOP;
+	_sprites[SP_PLAYER_LEFT1].sheet = &_sheets[SH_PLAYER];
+	_sprites[SP_PLAYER_LEFT1].generateFrames(SPRITE_SIZE, SPRITE_SIZE,
+		SP_PLAYER_LEFT1);
+
+	_sprites[SP_PLAYER_LEFT2].frames = PLAYER_FRAMES;
+	_sprites[SP_PLAYER_LEFT2].duration = PLAYER_DURATION;
+	_sprites[SP_PLAYER_LEFT2].loop = PLAYER_LOOP;
+	_sprites[SP_PLAYER_LEFT2].sheet = &_sheets[SH_PLAYER];
+	_sprites[SP_PLAYER_LEFT2].generateFrames(SPRITE_SIZE, SPRITE_SIZE,
+		SP_PLAYER_LEFT2);
+
+	_sprites[SP_PLAYER_LEFT3].frames = PLAYER_FRAMES;
+	_sprites[SP_PLAYER_LEFT3].duration = PLAYER_DURATION;
+	_sprites[SP_PLAYER_LEFT3].loop = PLAYER_LOOP;
+	_sprites[SP_PLAYER_LEFT3].sheet = &_sheets[SH_PLAYER];
+	_sprites[SP_PLAYER_LEFT3].generateFrames(SPRITE_SIZE, SPRITE_SIZE,
+		SP_PLAYER_LEFT3);
+
+	_sprites[SP_PLAYER_RIGHT1].frames = PLAYER_FRAMES;
+	_sprites[SP_PLAYER_RIGHT1].duration = PLAYER_DURATION;
+	_sprites[SP_PLAYER_RIGHT1].loop = PLAYER_LOOP;
+	_sprites[SP_PLAYER_RIGHT1].sheet = &_sheets[SH_PLAYER];
+	_sprites[SP_PLAYER_RIGHT1].generateFrames(SPRITE_SIZE, SPRITE_SIZE,
+		SP_PLAYER_RIGHT1);
+
+	_sprites[SP_PLAYER_RIGHT2].frames = PLAYER_FRAMES;
+	_sprites[SP_PLAYER_RIGHT2].duration = PLAYER_DURATION;
+	_sprites[SP_PLAYER_RIGHT2].loop = PLAYER_LOOP;
+	_sprites[SP_PLAYER_RIGHT2].sheet = &_sheets[SH_PLAYER];
+	_sprites[SP_PLAYER_RIGHT2].generateFrames(SPRITE_SIZE, SPRITE_SIZE,
+		SP_PLAYER_RIGHT2);
+
+	_sprites[SP_PLAYER_RIGHT3].frames = PLAYER_FRAMES;
+	_sprites[SP_PLAYER_RIGHT3].duration = PLAYER_DURATION;
+	_sprites[SP_PLAYER_RIGHT3].loop = PLAYER_LOOP;
+	_sprites[SP_PLAYER_RIGHT3].sheet = &_sheets[SH_PLAYER];
+	_sprites[SP_PLAYER_RIGHT3].generateFrames(SPRITE_SIZE, SPRITE_SIZE,
+		SP_PLAYER_RIGHT3);
+
+	_sprites[SP_PLAYER_HIT].frames = SP_PLAYER_HIT_FRAMES;
+	_sprites[SP_PLAYER_HIT].sheet = &_sheets[SH_PLAYER];
+	_sprites[SP_PLAYER_HIT].generateFrames(SPRITE_SIZE, SPRITE_SIZE,
+		SP_PLAYER_HIT);
+
+	_sprites[SP_PLAYER_GROUND].frames = SP_PLAYER_GROUND_FRAMES;
+	_sprites[SP_PLAYER_GROUND].sheet = &_sheets[SH_PLAYER];
+	_sprites[SP_PLAYER_GROUND].generateFrames(SPRITE_SIZE, SPRITE_SIZE,
+		SP_PLAYER_GROUND);
+
+	/* Obstacle Sprites */
+
+	_sprites[SP_OBSTACLE1].frames = OBSTACLE_FRAMES;
+	_sprites[SP_OBSTACLE1].sheet = &_sheets[SH_OBSTACLES];
+	_sprites[SP_OBSTACLE1].generateFrames(SPRITE_SIZE, SPRITE_SIZE, SP_OBSTACLE1);
+
+	_sprites[SP_OBSTACLE2].frames = OBSTACLE_FRAMES;
+	_sprites[SP_OBSTACLE2].sheet = &_sheets[SH_OBSTACLES];
+	_sprites[SP_OBSTACLE2].generateFrames(SPRITE_SIZE, SPRITE_SIZE, SP_OBSTACLE2);
+
+	_sprites[SP_OBSTACLE3].frames = OBSTACLE_FRAMES;
+	_sprites[SP_OBSTACLE3].sheet = &_sheets[SH_OBSTACLES];
+	_sprites[SP_OBSTACLE3].generateFrames(SPRITE_SIZE, SPRITE_SIZE, SP_OBSTACLE3);
+
+	_sprites[SP_OBSTACLE4].frames = OBSTACLE_FRAMES;
+	_sprites[SP_OBSTACLE4].sheet = &_sheets[SH_OBSTACLES];
+	_sprites[SP_OBSTACLE4].generateFrames(SPRITE_SIZE, SPRITE_SIZE, SP_OBSTACLE4);
+
+	/* Goat Sprites */
+
+	_sprites[SP_GOAT_LEFT].frames = SP_GOAT_LEFT_FRAMES;
+	_sprites[SP_GOAT_LEFT].duration = SP_GOAT_LEFT_DURATION;
+	_sprites[SP_GOAT_LEFT].loop = SP_GOAT_LEFT_LOOP;
+	_sprites[SP_GOAT_LEFT].sheet = &_sheets[SH_GOAT];
+	_sprites[SP_GOAT_LEFT].generateFrames(SPRITE_SIZE, SPRITE_SIZE, SP_GOAT_LEFT);
+
+	_sprites[SP_GOAT_RIGHT].frames = SP_GOAT_RIGHT_FRAMES;
+	_sprites[SP_GOAT_RIGHT].duration = SP_GOAT_RIGHT_DURATION;
+	_sprites[SP_GOAT_RIGHT].loop = SP_GOAT_RIGHT_LOOP;
+	_sprites[SP_GOAT_RIGHT].sheet = &_sheets[SH_GOAT];
+	_sprites[SP_GOAT_RIGHT].generateFrames(SPRITE_SIZE, SPRITE_SIZE,
+		SP_GOAT_RIGHT);
+
+	/* Sign Sprites */
+
+	_sprites[SP_SIGN1].frames = SIGN_FRAMES;
+	_sprites[SP_SIGN1].sheet = &_sheets[SH_SIGNS];
+	_sprites[SP_SIGN1].generateFrames(SIGN_WIDTH, SIGN_HEIGHT, SP_SIGN1);
+
+	_sprites[SP_SIGN2].frames = SIGN_FRAMES;
+	_sprites[SP_SIGN2].sheet = &_sheets[SH_SIGNS];
+	_sprites[SP_SIGN2].generateFrames(SIGN_WIDTH, SIGN_HEIGHT, SP_SIGN2);
+
+	_sprites[SP_SIGN3].frames = SIGN_FRAMES;
+	_sprites[SP_SIGN3].sheet = &_sheets[SH_SIGNS];
+	_sprites[SP_SIGN3].generateFrames(SIGN_WIDTH, SIGN_HEIGHT, SP_SIGN3);
+
+	_sprites[SP_SIGN4].frames = SP_SIGN4_FRAMES;
+	_sprites[SP_SIGN4].duration = SP_SIGN4_DURATION;
+	_sprites[SP_SIGN4].loop = SP_SIGN4_LOOP;
+	_sprites[SP_SIGN4].sheet = &_sheets[SH_ANIM_SIGN];
+	_sprites[SP_SIGN4].generateFrames(SIGN_WIDTH, SIGN_HEIGHT, SP_SIGN4);
 }
 
 Data* Data::_instance = nullptr;
--- src/data.h
+++ src/data.h
@@ -15,6 +15,9 @@ public:
 	// Returns a pointer to the sprite specified.
 	inline Sprite* sprite(int index) { return &_sprites[index]; }
 
+	// Returns a pointer to the SpriteSheet specified.
+	inline SpriteSheet* sheet(int index) { return &_sheets[index]; }
+
 private:
 	Data() { }
 
@@ -22,10 +25,10 @@ private:
 	static Data* _instance;
 
 	// Array of sprite sheets.
-	SpriteSheet _sheets[1];
+	SpriteSheet _sheets[6];
 
 	// Array of sprites.
-	Sprite _sprites[1];
+	Sprite _sprites[19];
 };
 
 /* Sheets */
@@ -33,14 +36,76 @@ private:
 #define SH_PLAYER 0
 #define SH_PLAYER_FILE "data/player.bmp"
 
-/* Sprites */
+#define SH_GOAT 1
+#define SH_GOAT_FILE "data/goat.bmp"
+
+#define SH_OBSTACLES 2
+#define SH_OBSTACLES_FILE "data/obstacles.bmp"
+
+#define SH_SIGNS 3
+#define SH_SIGNS_FILE "data/signs.bmp"
+
+#define SH_ANIM_SIGN 4
+#define SH_ANIM_SIGN_FILE "data/sign-animated.bmp"
+
+#define SH_FONT 5
+#define SH_FONT_FILE "data/font.bmp"
+
+/* Player Sprites */
 
 #define SPRITE_SIZE 32
+#define PLAYER_FRAMES 6
+#define PLAYER_DURATION 0.9f
+#define PLAYER_LOOP true
 
 #define SP_PLAYER_DOWN 0
-#define SP_PLAYER_DOWN_ROW 0
-#define SP_PLAYER_DOWN_FRAMES 6
-#define SP_PLAYER_DOWN_DURATION 0.9f
-#define SP_PLAYER_DOWN_LOOP true
+#define SP_PLAYER_LEFT1 1
+#define SP_PLAYER_LEFT2 2
+#define SP_PLAYER_LEFT3 3
+#define SP_PLAYER_RIGHT1 4
+#define SP_PLAYER_RIGHT2 5
+#define SP_PLAYER_RIGHT3 6
+
+#define SP_PLAYER_HIT 7
+#define SP_PLAYER_HIT_FRAMES 1
+
+#define SP_PLAYER_GROUND 8
+#define SP_PLAYER_GROUND_FRAMES 1
+
+/* Obstacle Sprites */
+
+#define OBSTACLE_FRAMES 1
+
+#define SP_OBSTACLE1 9
+#define SP_OBSTACLE2 10
+#define SP_OBSTACLE3 11
+#define SP_OBSTACLE4 12
+
+/* Goat Sprites */
+
+#define SP_GOAT_LEFT 13
+#define SP_GOAT_LEFT_FRAMES 7
+#define SP_GOAT_LEFT_DURATION 1.5f
+#define SP_GOAT_LEFT_LOOP true
+
+#define SP_GOAT_RIGHT 14
+#define SP_GOAT_RIGHT_FRAMES 7
+#define SP_GOAT_RIGHT_DURATION 1.5f
+#define SP_GOAT_RIGHT_LOOP true
+
+/* Sign Sprites */
+
+#define SIGN_WIDTH 100
+#define SIGN_HEIGHT 64
+#define SIGN_FRAMES 1
+
+#define SP_SIGN1 15
+#define SP_SIGN2 16
+#define SP_SIGN3 17
+
+#define SP_SIGN4 18
+#define SP_SIGN4_FRAMES 30
+#define SP_SIGN4_DURATION 9.0f
+#define SP_SIGN4_LOOP true
 
 #endif
